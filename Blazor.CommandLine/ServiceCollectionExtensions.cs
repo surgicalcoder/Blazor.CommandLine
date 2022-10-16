edfbad549b88da3e77400882590681d964c0adb8
Monty <ma@surgicalcoder.com>
2022-10-16 15:09:42 +0100

    Try to patch with harmony

--- Blazor.CommandLine/ServiceCollectionExtensions.cs
+++ Blazor.CommandLine/ServiceCollectionExtensions.cs
@@ -1,4 +1,7 @@
+using System.CommandLine;
+using System.Reflection;
 using Blazor.CommandLine.Command;
+using HarmonyLib;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Blazor.CommandLine;
@@ -7,7 +10,6 @@ public static class ServiceCollectionExtensions
 {
     public static IServiceCollection AddCommandLine(this IServiceCollection serviceCollection,Action<CommandLineOptions> options=null)
     {
-
         serviceCollection.AddTransient<IRunningCommand, RunningCommand>();
         var consoleOptions = new CommandLineOptions();
 
@@ -16,7 +18,29 @@ public static class ServiceCollectionExtensions
             options.Invoke(consoleOptions);
         }
 
+        PerformHarmonyPatch();
+        
         return serviceCollection;
+    }
+
+    private static void PerformHarmonyPatch()
+    {
+        var harmony = new Harmony("com.blazor.console.resetcolorpatch");
+
+        var mOriginal = typeof(ConsoleExtensions).Assembly.GetType("System.CommandLine.IO.ConsoleExtensions").GetMethod("ResetTerminalForegroundColor", BindingFlags.Static | BindingFlags.NonPublic);
+        var mPrefix = typeof(UnholyPatch).GetMethod("ResetTerminalForegroundColor", BindingFlags.Static | BindingFlags.NonPublic);
+        
+        // add null checks here
+
+        harmony.Patch(mOriginal, new HarmonyMethod(mPrefix));
+    }
+
+    private static class UnholyPatch
+    {
+        private static void ResetTerminalForegroundColor(IConsole console, ref bool __result)
+        {
+            __result = false;
+        }
     }
 
     public class CommandLineOptions
